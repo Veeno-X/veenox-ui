d3a31c1eeb8c79b08a03f9b508c9615c948501bc
Veeno <sebastien.cloiseau@gmail.com>
2024-12-06 00:57:34 +0100

    feature: header

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -6,13 +6,13 @@ export const Header = () => {
   const scrollDirection = useScrollDirection();
   return (
     <div
-      className={`fixed top-9 max-w-[1000px]  left-1/2 -translate-x-1/2 h-[60px] md:h-[80px] z-[100] w-[90%]
-   bg-[#2b2f3649] border border-borderColor-DARK rounded-full
-   backdrop-blur-md px-5 md:px-5 flex items-center justify-between transition-all duration-300
+      className={`fixed top-9 max-w-[1000px]  left-1/2 -translate-x-1/2 h-[60px] md:h-[70px] z-[1000] w-[90%]
+   bg-[#2b2f3649] border border-borderColor-DARK rounded-xl
+   backdrop-blur-md px-5 md:pl-5 pr-2 flex items-center justify-between transition-all duration-300
    ${scrollDirection === "down" ? "-translate-y-[120px]" : "translate-y-0"}`}
     >
       <img
-        src="/veenox/veenox-text.png"
+        src="/partners/veenox.png"
         alt="Veeno Logo"
         className="h-[30px] md:h-[35px] md:block hidden w-auto max-w-auto max-h-[25px] sm:max-w-auto md:max-h-[35px]"
       />
@@ -22,7 +22,7 @@ export const Header = () => {
         className="h-[27px] md:hidden block w-auto max-w-auto max-h-[30px] sm:max-w-auto"
       />
       <nav className="h-full cursor-pointer">
-        <ul className="text-white text-medium text-sm md:text-base flex items-center gap-2.5 md:gap-5 h-full">
+        <ul className="text-white font-bold text-sm md:text-base flex items-center gap-2.5 md:gap-5 h-full">
           <li>
             {" "}
             <Link
@@ -32,17 +32,6 @@ export const Header = () => {
               className="w-full h-full hover:text-base_color"
             >
               Twitter
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link
-              href="https://t.me/veenox_xyz"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="w-full h-full hover:text-base_color"
-            >
-              Telegram
             </Link>
           </li>
           <li>
@@ -67,13 +56,13 @@ export const Header = () => {
           </li>
         </ul>
       </nav>
-      <button className="h-[35px] md:h-[45px] px-1 lg:px-3 rounded-full text-white text-sm lg:text-lg cursor-pointer bg-base_color sm:block hidden">
+      <button className="h-[35px] md:h-[45px] px-1 lg:px-3 rounded-xl text-white text-sm lg:text-lg cursor-pointer bg-base_color sm:block hidden bg-purple">
         <Link
           href="/perp/PERP_BTC_USDC"
           className="w-full h-full hover:text-white"
         >
-          <div className="flex items-center justify-center w-full text-sm lg:text-base h-full px-3 lg:px-4 py-2">
-            Launch Alpha
+          <div className="flex items-center font-bold justify-center w-full text-sm lg:text-base h-full px-3 lg:px-4 py-2">
+            Start Trading
           </div>
         </Link>
       </button>
